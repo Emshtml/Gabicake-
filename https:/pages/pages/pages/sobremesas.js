31ff07d1eec8729edad2fb87b3d2021f792b1b84
Emshtml <casadathereza1@gmail.com>
2025-09-29 11:49:54 -0300

    Create sobremesas.js

---
+++ https:/pages/pages/pages/sobremesas.js
@@ -0,0 +1,8 @@
+export default function Sobremesas() {
+  return (
+    <div>
+      <h1>Sobremesas 🍮</h1>
+      <p>Delícias doces para qualquer ocasião, perfeitas para finalizar sua refeição.</p>
+    </div>
+  );
+}

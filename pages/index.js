45d8c0f21e4715432573e1e5f4730d69061b8e97
Emshtml <casadathereza1@gmail.com>
2025-09-29 00:21:51 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+// pages/index.js
+
+export default function Home() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column' }}>
+      <h1>Bem-vindo ao GabicakeApp!</h1>
+      <p>Seu aplicativo Next.js está rodando no Vercel 🚀</p>
+    </div>
+  );
+}

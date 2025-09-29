d36ee5963982eb0c3e589132b29263a84c1c6a70
Emshtml <casadathereza1@gmail.com>
2025-09-29 11:52:13 -0300

    Create bolos-de-potes.js

---
+++ https:/pages/pages/pages/pages/bolos-de-potes.js
@@ -0,0 +1,8 @@
+export default function BolosDePotes() {
+  return (
+    <div>
+      <h1>Bolos de Potes 🥣</h1>
+      <p>Bolos práticos e saborosos em potes individuais, ideais para presente ou consumo rápido.</p>
+    </div>
+  );
+}

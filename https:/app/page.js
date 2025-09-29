3f4a6a3aa0e406ce6255dd25cc8ce9b998dfd532
Emshtml <casadathereza1@gmail.com>
2025-09-29 01:48:07 -0300

    Create page.js

---
+++ https:/app/page.js
@@ -0,0 +1,9 @@
+// app/page.js
+export default function Page() {
+  return (
+    <div>
+      <p>Deploy funcionando com sucesso 🚀</p>
+      <p>Acesse a API: <code>/api/hello</code></p>
+    </div>
+  );
+}

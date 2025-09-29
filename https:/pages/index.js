45bd42ca7fb9e3051eb32c382cf1b5df50f7fce9
Emshtml <casadathereza1@gmail.com>
2025-09-29 11:48:00 -0300

    Create index.js

---
+++ https:/pages/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Gabicake App 🍰</h1>
+      <p>Deploy funcionando com sucesso 🚀</p>
+      <p>Escolha uma categoria:</p>
+      <ul>
+        <li><a href="/bolos">Bolos</a></li>
+        <li><a href="/sobremesas">Sobremesas</a></li>
+        <li><a href="/bolos-de-potes">Bolos de Potes</a></li>
+        <li><a href="/doces-de-conservas">Doces de Conservas</a></li>
+      </ul>
+    </div>
+  );
+}

ffd294cee99809ce90e43c8c665b49348b0f60f6
Emshtml <casadathereza1@gmail.com>
2025-09-29 11:51:07 -0300

    Create doces-de-conservas.js

---
+++ https:/pages/pages/pages/doces-de-conservas.js
@@ -0,0 +1,8 @@
+export default function DocesDeConservas() {
+  return (
+    <div>
+      <h1>Doces de Conservas 🍯</h1>
+      <p>Doces caseiros em conserva, perfeitos para levar para casa ou presentear.</p>
+    </div>
+  );
+}

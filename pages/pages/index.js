3e3485282de5502dfedc6b35f413d2a9870a40a6
Emshtml <casadathereza1@gmail.com>
2025-09-29 21:09:38 -0300

    Create index.js

---
+++ pages/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>🍰 Bem-vindo à GabiCake!</h1>
+      <p>Nosso site está funcionando corretamente 🚀</p>
+    </div>
+  );
+}

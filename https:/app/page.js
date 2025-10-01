161a07977c1747e19d3d826c87ce7e519c757e52
Emshtml <casadathereza1@gmail.com>
2025-10-01 16:39:09 -0300

    Delete https:/app directory

--- https:/app/page.js
+++
@@ -1,9 +0,0 @@
-// app/page.js
-export default function Page() {
-  return (
-    <div>
-      <p>Deploy funcionando com sucesso 🚀</p>
-      <p>Acesse a API: <code>/api/hello</code></p>
-    </div>
-  );
-}

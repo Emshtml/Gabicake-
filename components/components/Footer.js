0a83ca32b40ad016caa483e25cf3495c439c9cad
Emshtml <casadathereza1@gmail.com>
2025-10-08 01:15:45 -0300

    Create Footer.js

---
+++ components/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ textAlign: "center", padding: "1rem" }}>
+      <p>© 2025 Gabicake - Todos os direitos reservados 🍰</p>
+    </footer>
+  );
+}
